833249b7ee3a8d2a888802ab6205be897f49a7fa
ImChikku <111957486+ImChikku@users.noreply.github.com>
2023-01-03 17:34:22 +0530

    Add files via upload

---
+++ sql/anbu.sql
@@ -0,0 +1,130 @@
+CREATE TABLE `contact` (
+  `Name` varchar(250) NOT NULL,
+  `Email` varchar(250) NOT NULL,
+  `Mobile` varchar(250) NOT NULL,
+  `Subject` varchar(250) NOT NULL,
+  `Message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `customer` (
+  `rollno` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `customer` (`rollno`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('18suca043', 'anbu', 'anbumurugan@gmail.com', '9876543210', '26,bigwellstreet,', '123456');
+
+CREATE TABLE `food` (
+  `F_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `R_ID` int(30) NOT NULL,
+  `images_path` varchar(200) NOT NULL,
+  `options` varchar(10) NOT NULL DEFAULT 'ENABLE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `food` (`F_ID`, `name`, `price`, `description`, `R_ID`, `images_path`, `options`) VALUES
+
+(1, 'samosa', 10, 'fantacy taste', 1, 'images/samosa.jpg', 'ENABLE'),(2, 'Pizza', 10, 'fantacy taste', 1, 'images/Pizza.jpg', 'ENABLE'),(3, 'tea', 10, 'fantacy taste', 1, 'images/tea.jpg', 'ENABLE'),(4, 'Coffee', 10, 'fantacy taste', 1, 'images/coffee.jpg', 'ENABLE'),(5, 'Pepsi', 10, 'Soft_Drinks', 1, 'images/Pepsi.jpg', 'ENABLE'),(6, 'Coke', 10, 'Soft_Drinks', 1, 'images/cola.jpg', 'ENABLE');
+
+CREATE TABLE `manager` (
+  `username` varchar(30) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `manager` (`username`, `fullname`, `email`, `contact`, `address`, `password`) VALUES
+('kutty', 'mathan', 'matmathan2001@gmail.com', '9344240352', '1/1999/2,sathiya sai nagar,pandiyan nagar', 'appa9927');
+
+
+CREATE TABLE `orders` (
+  `order_ID` int(30) NOT NULL,
+  `F_ID` int(30) NOT NULL,
+  `foodname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `R_ID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `canteens` (
+  `R_ID` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `M_ID` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `canteens` (`R_ID`, `name`, `email`, `contact`, `address`, `M_ID`) VALUES
+(1, 'LaKshu Canteen', 'matmathan2001@gmail.com', '9344240352', '1/1999/2,sathiya sai nagar,pandiyan nagar', 'kutty');
+
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`rollno`);
+
+
+
+
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`F_ID`,`R_ID`),
+  ADD KEY `R_ID` (`R_ID`);
+
+
+
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`username`);
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `F_ID` (`F_ID`),
+  ADD KEY `username` (`username`),
+  ADD KEY `R_ID` (`R_ID`);
+ALTER TABLE `canteens`
+  ADD PRIMARY KEY (`R_ID`),
+  ADD UNIQUE KEY `M_ID_2` (`M_ID`),
+  ADD KEY `M_ID` (`M_ID`);
+
+
+
+
+ALTER TABLE `food`
+  MODIFY `F_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+
+
+ALTER TABLE `orders`
+  MODIFY `order_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+
+
+ALTER TABLE `canteens`
+  MODIFY `R_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `canteen` (`R_ID`);
+
+
+
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `food` (`F_ID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`username`) REFERENCES `customer` (`username`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`R_ID`) REFERENCES `canteen` (`R_ID`);
+
+
+ALTER TABLE `canteens`
+  ADD CONSTRAINT `canteen_ibfk_1` FOREIGN KEY (`M_ID`) REFERENCES `manager` (`username`);
+COMMIT;
+
